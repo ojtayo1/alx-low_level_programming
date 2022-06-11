4b67aca4b217afbea797337894ee52a6993eabe6
ojtayo1 <sureboi.oj@gmail.com>
2022-06-11 06:51:46 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,19 @@
 int main() 
 {
 	char ch = ""
-	putchar (ch);
+	putchar (ch)#include "main.h"                                                                                        
+/**
+ * main - print putchar
+ *  Return: Always 0
+ */
+int main()
+{
+        char ch = ""
+        putchar (ch);
+
+        return (0);
+}
+;
 
 	return (0);
 }
